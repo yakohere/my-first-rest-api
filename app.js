f9ceec9b3fb7958eb2e6c6005e1c2255de20500b
devyako <yakhyoismoildjonov@gmail.com>
2020-05-01 15:49:49 +0500

    finish img uploading

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+require("dotenv").config();
+
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
-app.use(bodyParser.json());
+const uri = process.env.mongodb_uri;
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -18,4 +53,16 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
