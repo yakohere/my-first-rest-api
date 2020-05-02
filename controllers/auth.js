d2d7db6381f2a2a8c3fc56217b01efeabdfeaa09
devyako <yakhyoismoildjonov@gmail.com>
2020-05-03 00:32:03 +0500

    sign up finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -10,6 +11,25 @@ exports.signup = (req, res, next) => {
     throw error;
   }
   const email = req.body.email;
-  const password = req.body.email;
-  const name = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPw) => {
+      const user = new User({
+        email: email,
+        password: hashedPw,
+        name: name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "User created!", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
