8b1445ad052ac1b8c0031482ca77bca64828e1ba
devyako <yakhyoismoildjonov@gmail.com>
2020-05-03 15:31:17 +0500

    socket io finished

--- app.js
+++ app.js
@@ -66,7 +66,11 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(uri)
   .then(() => {
-    app.listen(8080);
-    console.log("Connected!!!");
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client Connected!!!");
+    });
+    console.log("Database Connected!!!");
   })
   .catch((err) => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const { validationResult } = require("express-validator/check");
+const io = require("../socket");
 
 const Post = require("../models/post");
 const User = require("../models/user");
@@ -54,6 +55,7 @@ exports.createPost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
+    io.getIO().emit("posts", { action: "create", post: post });
     res.status(201).json({
       message: "Post created successfully!",
       post: post,
